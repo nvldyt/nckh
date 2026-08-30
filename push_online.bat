2536cfd2da5ee735e606555d82c516c151c85fdc
Văn Linh <140031769+nvldyt@users.noreply.github.com>
2026-08-30 23:31:08 +0700

    Create push_online.bat

---
+++ push_online.bat
@@ -0,0 +1,13 @@
+@echo off
+color 0B
+echo ===================================================
+echo   DANG LUU VA DAY CODE TU MAY TINH LEN GITHUB...
+echo ===================================================
+
+git add .
+git commit -m "Cap nhat code tu Local"
+git push origin main
+
+echo.
+echo DA DAY THANH CONG! STREAMLIT ONLINE SE TU CAP NHAT.
+pause
